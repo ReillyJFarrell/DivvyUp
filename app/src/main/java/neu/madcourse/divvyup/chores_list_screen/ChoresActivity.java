18ea1b9221dd5f50aa84ad2ac6beba28e501626d
ericli0303 <ericli0303@gmail.com>
2022-04-17 23:58:29 -0400

    progress with progress bar

--- app/src/main/java/neu/madcourse/divvyup/chores_list_screen/ChoresActivity.java
+++ app/src/main/java/neu/madcourse/divvyup/chores_list_screen/ChoresActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -23,7 +31,7 @@ public class ChoresActivity extends AppCompatActivity {
     private RecyclerView toDoRView;
     private RecyclerView inProgressRView;
     private RecyclerView completedRView;
-    private BarChart stackedChart;
+    private BarChart chart;
 
     private ChoreAdapter toDoChoreAdapter;
     private ChoreAdapter inProgressChoreAdapter;
@@ -34,11 +42,21 @@ public class ChoresActivity extends AppCompatActivity {
     private List<ChoreCard> inProgressChoresList = new ArrayList<>();
     private List<ChoreCard> completedChoresList = new ArrayList<>();
 
+    int[] colorClassArray = new int[]{Color.RED, Color.CYAN, Color.RED};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chores);
+
+        chart = findViewById(R.id.idBarChart);
+
+        BarDataSet barDataSet = new BarDataSet(dataValues1(), "Bar Set");
+        barDataSet.setColor(Color.CYAN);
+
+        BarData barData = new BarData(barDataSet);
+        chart.setData(barData);
 
         toDoChoresList.add(new ChoreCard(3, 1, "Dishes", "clean dishes", new Date(2022, 5, 12), 1));
         toDoChoresList.add(new ChoreCard(2, 1, "Trash", "clean dishes", new Date(2022, 5, 12), 1));
@@ -109,6 +127,12 @@ public class ChoresActivity extends AppCompatActivity {
         completedRView.setLayoutManager(completedLayoutManager);
         completedRView.setHasFixedSize(true);
 
+    }
+
+    private ArrayList<BarEntry> dataValues1() {
+        ArrayList<BarEntry> dataVals = new ArrayList<>();
+        dataVals.add(new BarEntry(0, new float[]{2, 5.5f, 4}));
+        return dataVals;
     }
 
 
