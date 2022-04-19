b4c1bad0285f93360bd8d000a90fd86c172e1c46
jay <notactuallyjay@gmail.com>
2022-04-19 14:42:50 -0400

    edit chore page design

---
+++ app/src/main/java/neu/madcourse/divvyup/EditChoreActivity.java
@@ -0,0 +1,14 @@
+package neu.madcourse.divvyup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditChoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_chore);
+    }
+}
