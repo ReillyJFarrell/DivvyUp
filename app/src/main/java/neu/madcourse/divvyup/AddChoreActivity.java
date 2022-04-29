e053c3b98d881340bb127ebed5a271ed1a3bea72
jay <notactuallyjay@gmail.com>
2022-04-28 21:28:59 -0400

    add chore (rn its just a copy of edit)

---
+++ app/src/main/java/neu/madcourse/divvyup/AddChoreActivity.java
@@ -0,0 +1,14 @@
+package neu.madcourse.divvyup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddChoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_chore);
+    }
+}
