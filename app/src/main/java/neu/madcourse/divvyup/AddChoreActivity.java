01260aa940ab573612ed73dbf6cacb187e955b18
jay <notactuallyjay@gmail.com>
2022-04-30 13:18:19 -0400

    add chore refactor

--- app/src/main/java/neu/madcourse/divvyup/AddChoreActivity.java
+++ app/src/main/java/neu/madcourse/divvyup/AddChoreActivity.java
@@ -3,12 +3,94 @@ package neu.madcourse.divvyup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class AddChoreActivity extends AppCompatActivity {
+
+    EditText choreName;
+    Spinner assignedSpinner;
+
+    CheckBox sunday;
+    CheckBox monday;
+    CheckBox tuesday;
+    CheckBox wednesday;
+    CheckBox thursday;
+    CheckBox friday;
+    CheckBox saturday;
+
+    ArrayList<CheckBox> week;
+
+    CheckBox repeating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_chore);
+
+        choreName = findViewById(R.id.addChoreNameEditView);
+        // need to read from DB
+        assignedSpinner = (Spinner) findViewById(R.id.addAssignedSpinner);
+        // users array should be dynamic, not static
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.users_array, R.layout.spinner_selected_item);
+        ArrayList<String> users = new ArrayList<>();
+        users.add("User 1");
+        users.add("User 2");
+        users.add("User 3");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_selected_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        assignedSpinner.setAdapter(adapter);
+
+        sunday = findViewById(R.id.addSundayCheckbox);
+        week.add(sunday);
+        monday = findViewById(R.id.addMondayCheckbox);
+        week.add(monday);
+        tuesday = findViewById(R.id.addTuesdayCheckbox);
+        week.add(tuesday);
+        wednesday = findViewById(R.id.addWednesdayCheckbox);
+        week.add(wednesday);
+        thursday = findViewById(R.id.addThursdayCheckbox);
+        week.add(thursday);
+        friday = findViewById(R.id.addFridayCheckbox);
+        week.add(friday);
+        saturday = findViewById(R.id.addSaturdayCheckbox);
+        week.add(saturday);
+
+        repeating = findViewById(R.id.addRepeatingCheckBox);
+
+        // should be getting this from bundle extras
+        String groupID = "testID";
+        String choreID = "choreID";
+    }
+
+    private void addChore(String groupID) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("groups").child(groupID).child("chores");
+        DatabaseReference postRef = mDatabase.push();
+
+        mDatabase.child("userAssigned").setValue(choreName.getText().toString());
+
+        String assignedUser = assignedSpinner.getSelectedItem().toString();
+        mDatabase.child("userAssigned").setValue(assignedUser);
+
+        ArrayList<Boolean> days = new ArrayList<Boolean>();
+        for (int i = 0; i < week.size(); i++) {
+            if (week.get(i).isChecked()) {
+                days.add(true);
+            }
+            else {
+                days.add(false);
+            }
+        }
+        mDatabase.child("days").setValue(days);
+
+        mDatabase.child("isRepeat").setValue(repeating.isChecked());
+
     }
 }
