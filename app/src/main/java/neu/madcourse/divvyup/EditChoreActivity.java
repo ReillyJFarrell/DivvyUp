e6ec8bd1c4d691ed5e1d854e26c28ea7ca5cc339
jay <notactuallyjay@gmail.com>
2022-05-01 18:20:28 -0400

    edit chore statuses as well

--- app/src/main/java/neu/madcourse/divvyup/EditChoreActivity.java
+++ app/src/main/java/neu/madcourse/divvyup/EditChoreActivity.java
@@ -196,13 +196,16 @@ public class EditChoreActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
                 String assignedUser = assignedSpinner.getSelectedItem().toString();
+                ArrayList<Integer> statuses = new ArrayList<>();
                 ArrayList<Boolean> days = new ArrayList<Boolean>();
                 for (int i = 0; i < week.size(); i++) {
                     if (week.get(i).isChecked()) {
                         days.add(true);
+                        statuses.add(i, 0);
                     }
                     else {
                         days.add(false);
+                        statuses.add(i, -1);
                     }
                 }
 
@@ -210,7 +213,12 @@ public class EditChoreActivity extends AppCompatActivity {
                 //DataSnapshot snap = toSet.iterator().next();
                 //GroupObject currentGroup = snap.getValue(GroupObject.class);
 
-                ChoreObject editedChore = new ChoreObject(choreNameEditText.getText().toString(), groupID, choreID, assignedUser, days, repeating.isChecked());
+                ChoreObject editedChore = snapshot.getChildren().iterator().next().getValue(ChoreObject.class);//new ChoreObject(choreNameEditText.getText().toString(), groupID, choreID, assignedUser, days, repeating.isChecked());
+                editedChore.setName(choreNameEditText.getText().toString());
+                editedChore.setUserAssigned(assignedUser);
+                editedChore.setDays(days);
+                editedChore.setProgressMode(statuses);
+                editedChore.setRepeat(repeating.isChecked());
                 DatabaseReference newRef = FirebaseDatabase.getInstance().getReference().child("groups").child(groupID).child("chores").child(Integer.toString(position));
                 newRef.setValue(editedChore);
             }
